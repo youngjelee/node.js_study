a6585b63ce7474b6478f8472d76377ba406561be
youngjelee <wpdud1221@naver.com>
2021-10-30 14:24:53 +0900

    프론트 로그인 구현

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -22,5 +22,15 @@ const id = document.querySelector("#id"),
             "Content-Type" : "application/json",
         },
         body : JSON.stringify(req),
-    });
+    }).then((res) => res.json())
+      .then((res) => {
+          if(res.success) {
+              location.href="/";
+          }else{
+              alert(res.msg);
+          }
+      })
+      .catch((err) =>{
+          console.error(new Error("로그인중 에러발생"));
+      });
 };
--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -8,17 +8,45 @@ const output = {
     login  : (req,res) =>{
         res.render("home/login");
     }
+    
+    
+};
 
-}
+
+const users= {
+    id: ["wpdud1221", "test2" , "test3"],
+    pwd : ["pwd1","pwd2","pwd3"]
+};
 
 const process = {
-    login : (req,res) => {
-        console.log(req.body);
-    }
-}
+  
+    login : (req, res) => {
+        const id = req.body.id ,
+        pwd = req.body.pwd;
+
+        // console.log(id,pwd);
+
+        if(users.id.includes(id)){
+            const idx = users.id.indexOf(id);
+            if(users.pwd[idx] === pwd){
+                console.log("로그인성공");
+                return res.json({
+                    success: true,
+                });
+            }
+        }
+
+
+        return res.json ({
+            success : false,
+            msg : "로그인에 실패하셨습니다",
+            
+        });
+    },
+};
 
 module.exports = {
     output,
     process    
 
-}
+};
