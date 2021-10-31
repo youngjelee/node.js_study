1638a3a88afcd1df0a10bc9a9390f136b84cb9a9
youngjelee <wpdud1221@naver.com>
2021-10-31 16:03:43 +0900

    로그인 구현( 파일관련) 오류 생김 수정요

--- app/src/models/User.js
+++ app/src/models/User.js
@@ -10,18 +10,18 @@ class User{
     }
 
     login(){
-        // const {id , pwd}= UserStorage.getUsers("id","pwd");
-        const {id , pwd} = UserStorage.getUserInfo("wpdud1221");
+        //  UserStorage.getUsers("id","pwd");
+        UserStorage.getUserInfo(this.body.id);
         
-        if(id){
-            if(id ===this.body.id && pwd ===this.body.pwd){
-                return {success : true};                
-            }
-            return {success : false , msg: "비밀번호가 틀렸다"}
-        }
-        return  {success : false , msg: "존재하지 않는 아이디입니다."}
+        // if(id){
+        //     if(id ===this.body.id && pwd ===this.body.pwd){
+        //         return {success : true};                
+        //     }
+        //     return {success : false , msg: "비밀번호가 틀렸다"}
+        // }
+        // return  {success : false , msg: "존재하지 않는 아이디입니다."}
     }
-    
+
     register() {
         UserStorage.save(this.body);
     }
--- app/src/models/UserStorage.js
+++ app/src/models/UserStorage.js
@@ -1,33 +1,51 @@
 "use Strict";
 
+const e = require("express");
+const fs = require("fs").promises;
+
 class UserStorage {
-    static #users= {
-        id: ["wpdud1221", "test2" , "test3"],
-        pwd : ["pwd1","pwd2","pwd3"],
-        names:["영이","일이","삼이"]
-    };
+    // static #users= {
+    //     id: ["wpdud1221", "test2" , "test3"],
+    //     pwd : ["pwd1","pwd2","pwd3"],
+    //     names:["영이","일이","삼이"]
+    // };
 
     static getUsers(...fields){
-        const users = this.#users;
-        const newUsers = fields.reduce((newUsers,field) => {
-            if(users.hasOwnProperty(field)){
-                newUsers[field] = users[field];
-            }
-            return newUsers;
-        } , {});
-        return newUsers; 
+        // const users = this.#users;
+
+        // const newUsers = fields.reduce((newUsers,field) => {
+        //     if(users.hasOwnProperty(field)){
+        //         newUsers[field] = users[field];
+        //     }
+        //     return newUsers;
+        // } , {});
+        // return newUsers; 
     }
 
     static getUserInfo(id){
-        const users = this.#users;
-        const idx = users.id.indexOf(id);
-        const usersKeys = Object.keys(users);  // =>[id,pwd,name]
-        const userInfo = usersKeys.reduce((newUser,info) => {
+    //    console.log(fs.readFile("./src/databases/user.json"));
+           return fs
+           .readFile("./src/databases/user.json")
+            .then((data)=>{
+                if(err)  throw err;
+                // console.log(JSON.parse(data));
+                const users = JSON.parse(data);
+                const idx = users.id.indexOf(id);
+                const usersKeys = Object.keys(users);  // =>[id,pwd,name]
+                const userInfo = usersKeys.reduce((newUser,info) => {
+        
+                  newUser[info] =users[info][idx];
+                  return newUser;
+                }, {});
+                console.log(userInfo);
+                return userInfo;
+            })
+            .catch((err) =>console.error);
+        
+    }
 
-            newUser[info] =users[info][idx];
-            return newUser;
-        }, {});
-        return userInfo;
+    static save(userInfo){
+        
     }
 }
 
